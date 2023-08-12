5dbf104659caa899188c71377246c4d8659c28dc
Lee <flyingfireco@gmail.com>
2023-08-12 17:17:30 -0400

    added asymmetric sampler

--- tilingSampler.py
+++ tilingSampler.py
@@ -6,6 +6,69 @@ import comfy.utils
 import comfy.clip_vision
 import torch
 import nodes
+from typing import Optional
+
+class Asymmetric_Tiled_KSampler:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required":
+                {"model": ("MODEL", ),
+                 "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+                 "tileX": ("INT", {"default": 1, "min": 0, "max": 1}),
+                 "tileY": ("INT", {"default": 1, "min": 0, "max": 1}),
+                 "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+                 "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0}),
+                 "sampler_name": (comfy.samplers.KSampler.SAMPLERS, ),
+                 "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
+                 "positive": ("CONDITIONING", ),
+                 "negative": ("CONDITIONING", ),
+                 "latent_image": ("LATENT", ),
+                 "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
+                 }}
+
+    RETURN_TYPES = ("LATENT",)
+    FUNCTION = "sample"
+    CATEGORY = "Sampling/Tiled"
+
+    def apply_asymmetric_tiling(self, model, tileX, tileY):
+        for layer in [layer for layer in model.modules() if isinstance(layer, torch.nn.Conv2d)]:
+            layer.padding_modeX = 'circular' if tileX else 'constant'
+            layer.padding_modeY = 'circular' if tileY else 'constant'
+            layer.paddingX = (layer._reversed_padding_repeated_twice[0], layer._reversed_padding_repeated_twice[1], 0, 0)
+            layer.paddingY = (0, 0, layer._reversed_padding_repeated_twice[2], layer._reversed_padding_repeated_twice[3])
+            print(layer.paddingX, layer.paddingY)
+
+    def __hijackConv2DMethods(self, model, tileX: bool, tileY: bool):
+        for layer in [l for l in model.modules() if isinstance(l, torch.nn.Conv2d)]:
+            layer.padding_modeX = 'circular' if tileX else 'constant'
+            layer.padding_modeY = 'circular' if tileY else 'constant'
+            layer.paddingX = (layer._reversed_padding_repeated_twice[0], layer._reversed_padding_repeated_twice[1], 0, 0)
+            layer.paddingY = (0, 0, layer._reversed_padding_repeated_twice[2], layer._reversed_padding_repeated_twice[3])
+            
+            def make_bound_method(method, current_layer):
+                def bound_method(self, *args, **kwargs):  # Add 'self' here
+                    return method(current_layer, *args, **kwargs)
+                return bound_method
+                
+            bound_method = make_bound_method(self.__replacementConv2DConvForward, layer)
+            layer._conv_forward = bound_method.__get__(layer, type(layer))
+
+    def __replacementConv2DConvForward(self, layer, input: torch.Tensor, weight: torch.Tensor, bias: Optional[torch.Tensor]):
+        working = torch.nn.functional.pad(input, layer.paddingX, mode=layer.padding_modeX)
+        working = torch.nn.functional.pad(working, layer.paddingY, mode=layer.padding_modeY)
+        return torch.nn.functional.conv2d(working, weight, bias, layer.stride, (0, 0), layer.dilation, layer.groups)
+
+
+    def __restoreConv2DMethods(self, model):
+        for layer in [l for l in model.modules() if isinstance(l, torch.nn.Conv2d)]:
+            layer._conv_forward = torch.nn.Conv2d._conv_forward.__get__(layer, torch.nn.Conv2d)
+            
+
+    def sample(self, model, seed, tileX, tileY, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=1.0):
+        self.__hijackConv2DMethods(model.model, tileX == 1, tileY == 1)
+        result = nodes.common_ksampler(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise)
+        self.__restoreConv2DMethods(model.model)
+        return result
 
 class Tiled_KSampler:
     @classmethod
@@ -39,4 +102,5 @@ class Tiled_KSampler:
 
 NODE_CLASS_MAPPINGS = {
         "Tiled KSampler": Tiled_KSampler,
+        "Asymmetric Tiled KSampler": Asymmetric_Tiled_KSampler,
 }
